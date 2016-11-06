211ba6e61770a3e7c7f839e4f8a812474ca04f1e
ass233 <ass233.cm@163.com>
2016-11-06 23:16:47 +0800

    更新信息

--- JS/JavaScript基础/getStyle.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by 北狼 on 2016/8/26.
- */
-function getStyle(obj,sName) {
-    return obj.currentStyle?obj.currentStyle[sName]:getComputedStyle(obj,false)[sName];
-}
--- JS/Require/demo/num.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by 北狼 on 2016/9/30.
- */
-define([],function () {
-    return 10;
-});
